5295dc737af8bfcca37bbbf617d9b07f46c5630a
caoruixin <caoruixin@bytedance.com>
2024-11-23 22:52:10 +0800

    update default model and news' number

--- src/daemon_process.py
+++ src/daemon_process.py
@@ -108,7 +108,8 @@ def main():
 
     # 安排 Dogecoin 的定时任务
     schedule.every(4).hours.at(":00").do(dc_topic_job, dogecoin_news_client, report_generator)  # 每4小时执行一次
-    schedule.every().day.at("13:00").do(dc_daily_job, dogecoin_news_client, report_generator, notifier)  # 每天13点执行
+    schedule.every().day.at("08:30").do(dc_daily_job, dogecoin_news_client, report_generator, notifier)  # 每天13点执行
+    print("done")
 
 
     try:
